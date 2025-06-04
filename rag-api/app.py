5ef0cd2158fde56ae04301292193d9371678d0b6
WalidSyassi-azure <syassisyassi@gmail.com>
2025-06-04 16:33:23 +0100

    Update app.py

--- rag-api/app.py
+++ rag-api/app.py
@@ -21,7 +21,7 @@ try:
     embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
     vectorstore = FAISS.load_local("faiss_store", embeddings, allow_dangerous_deserialization=True)
     qa_chain = RetrievalQAWithSourcesChain.from_chain_type(
-        llm = OpenAI(temperature=0, api_key=OPENAI_API_KEY)
+        llm = OpenAI(temperature=0, api_key=OPENAI_API_KEY),
         chain_type="stuff",
         retriever=vectorstore.as_retriever()
     )
